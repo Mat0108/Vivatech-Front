d318cb9356c9fac1e06fcf2debbca7064d9d16e3
nihelouanassi <nihelouanasi@gmail.com>
2023-05-15 08:11:24 +0200

    add a pink row title for description section

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,9 @@ class ConferenceDetail extends StatelessWidget {
                   ),
                 ),
                 child: Column(
-                  children: [],
+                  children: [
+                    RowPink('Description'),
+                  ],
                 )),
           ])),
     );
@@ -124,6 +126,37 @@ class ConferenceDetail extends StatelessWidget {
         radius: radius,
         backgroundImage: const AssetImage("images/futur.png"),
       ),
+    );
+  }
+
+
+  Row RowPink(String text) {
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 10, top: 10),
+          width: 100,
+          height: 30,
+          decoration: BoxDecoration(
+            color: Colors.pink,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10.0),
+              bottomLeft: Radius.circular(10.0),
+              topRight: Radius.circular(10.0),
+              bottomRight: Radius.circular(10.0),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
