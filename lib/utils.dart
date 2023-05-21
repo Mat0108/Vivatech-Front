208e2530eb50a5055d337f4ca8495ca5ac859634
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-05-21 16:46:22 +0200

    Update utils.dart
    Add class MyCustomScrollBehavior

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
+}
+
