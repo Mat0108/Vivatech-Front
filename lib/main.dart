e235d94de024c9ad6ce47f6526c5d477efaa5ba1
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-05-21 16:57:37 +0200

    Update main.dart
    Add imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
+import 'package:myapp/page-1/accueil.dart';
+
 
 void main() {
   runApp(const MyApp());
