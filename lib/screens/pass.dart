e05cc3c59d45943f742dd17e210d3c6bd424c4b0
Tania Olivia <tania.olivia69@yahoo.com>
2023-05-21 03:29:03 +0200

    use the background and top navigation components

--- lib/screens/pass.dart
+++ lib/screens/pass.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, prefer_final_fields, avoid_print
 import 'package:flutter/material.dart';
+import 'package:vivatech/components/background.dart';
 import 'package:vivatech/components/qrCodeGenerator.dart';
+import 'package:vivatech/components/topNavigation.dart';
 
 class Pass extends StatefulWidget {
   @override
@@ -18,7 +20,7 @@ class _PassState extends State<Pass> {
         height: MediaQuery.of(context).size.height * 0.65,
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Image.asset(
-            "assets/pass/pass.png",
+            "assets/pages/home/pass.png",
             width: MediaQuery.of(context).size.width * 0.9,
           ),
           Padding(
@@ -71,52 +73,10 @@ class _PassState extends State<Pass> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/background/bg-gradient-vivatech-2.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(children: [
-        Row(children: [
-          Container(
-            alignment: Alignment.topCenter,
-            margin: EdgeInsets.fromLTRB(20, 40, 20, 40),
-            child: Row(
-              children: [
-                Container(
-                    height: 70,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(0, 255, 255, 1),
-                        border: Border.all(
-                            color: Color.fromRGBO(85, 8, 160, 0.3),
-                            width: 2,
-                            style: BorderStyle.solid),
-                        borderRadius: BorderRadius.circular(100)),
-                    child: Image.asset("assets/pass/pass.png")),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(255, 255, 255, 0.7),
-                      border:
-                          Border.all(color: Color.fromRGBO(0, 255, 255, 1))),
-                  child: Text("Mon Pass",
-                      style: TextStyle(
-                          color: Color.fromRGBO(85, 8, 160, 0.7),
-                          fontSize: 22,
-                          fontFamily: "MuseoSans",
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center),
-                ),
-              ],
-            ),
-          ),
-        ]),
-        GestureDetector(
+        body: BackgroundComponent(
+      content: TopNavigationComponent(
+        currentPage: "pass",
+        content: GestureDetector(
           child: AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (Widget child, Animation<double> animation) =>
@@ -124,7 +84,7 @@ class _PassState extends State<Pass> {
               child: _renderWidget()),
           onTap: () => {_updateWidget()},
         ),
-      ]),
+      ),
     ));
   }
 }
