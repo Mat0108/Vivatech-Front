eba33f0297c481d82577a6fd301f94c60344b707
Tania Olivia <tania.olivia69@yahoo.com>
2023-05-23 18:45:48 +0200

    add logic game

--- lib/screens/game_logic.dart
+++ lib/screens/game_logic.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, prefer_final_fields, avoid_print
 import 'package:flutter/material.dart';
 import 'package:vivatech/components/background.dart';
-import 'package:vivatech/components/gameContainer.dart';
+import 'package:vivatech/components/game/gameContainer.dart';
 import 'package:vivatech/components/topNavigation.dart';
 
 class GameLogic extends StatelessWidget {
+  bool clickedAnswer = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,20 +14,71 @@ class GameLogic extends StatelessWidget {
             content: Column(children: [
       TopNavigationComponent(currentPage: "game-logic"),
       Expanded(
-          child: GameContainerComponent(gameName: "game-logic", tutorial: [
-        'Bonjour! Bienvenue au jeu de la mémoire. Je suis Tania. Je suis ici pour vous guider avant commencer de jouer le jeu',
-        'Tout d’abord, vous verrez une question similaire à la question ci-dessous avec quelques images et une ou plusieurs cases manquantes.',
-        'Votre mission est de remplir cette case manquante en choisissant l’une des options.',
-        'Comme vous pouvez le voir la première boîte est Google, suivie par La Poste et un autre La Poste.',
-        'Logiquement, la première option, Orange n’est pas la bonne réponse parce que dans la question il n’y a pas Orange qui est utilisé.',
-        'La réponse est donc la deuxième ou la troisième option. Pour savoir quelle est la bonne réponse, nous devrons voir l’ordre des images.',
-        'Comme vous pouvez le voir, Google est la première commande.',
-        'Puis nous avons La Poste sur la deuxième et la troisième commande et il va continuer en boucle avec la même commande à chaque fois.',
-        'Cela signifie que la réponse appropriée à cette question est Google. Pour simplifier, nous dirons Google est 1 et La Poste, 2.',
-        'Ainsi, l’ordre sera : 1 - 2 - 2 - 1',
-        'Il y aura 10 questions par jeu et le niveau de difficulté pour chaque question sera plus élevé que la question précédente.',
-        'Donc, puisque j’ai déjà tout expliqué. Vous pouvez maintenant commencer votre mission. Bonne chance!'
-      ]))
+          child: GameContainerComponent(
+              content: Column(children: [
+        Padding(
+            padding: EdgeInsets.fromLTRB(0, 50, 0, 70),
+            child: Text("Question 1".toUpperCase(),
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white))),
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(10),
+          width: MediaQuery.of(context).size.width,
+          height: 100,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Image.asset("assets/pages/games/logic/amazon.png"),
+            Image.asset("assets/pages/games/logic/amazon.png"),
+            Image.asset("assets/pages/games/logic/ayming.png"),
+            Image.asset("assets/pages/games/logic/question.png")
+          ]),
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(20, 70, 20, 10),
+            child: Text(
+                "Choisissez l’une de ces options pour remplir la case manquante en fonction de l’ordre de la case manquante :",
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 15, color: Colors.white))),
+        Container(
+            width: MediaQuery.of(context).size.width,
+            height: 180,
+            padding: EdgeInsets.fromLTRB(20, 40, 20, 40),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    child: clickedAnswer
+                        ? Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.black,
+                                    width: 3,
+                                    style: BorderStyle.solid)),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                  "assets/pages/games/logic/amazon.png"),
+                            ))
+                        : ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                                "assets/pages/games/logic/amazon.png"),
+                          ),
+                    onTap: () => {clickedAnswer = !clickedAnswer},
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("assets/pages/games/logic/ayming.png"),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("assets/pages/games/logic/ikubator.png"),
+                  )
+                ]))
+      ]))),
     ])));
   }
 }
