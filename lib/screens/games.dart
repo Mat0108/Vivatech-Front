8169113ba15983475949d94863de60636ee04d8d
Tania Olivia <tania.olivia69@yahoo.com>
2023-05-23 18:26:01 +0200

    modify the redirection routes

--- lib/screens/games.dart
+++ lib/screens/games.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, prefer_final_fields, avoid_print
 import 'package:flutter/material.dart';
 import 'package:vivatech/components/background.dart';
-import 'package:vivatech/components/gamesContainer.dart';
+import 'package:vivatech/components/game/gamesContainer.dart';
 import 'package:vivatech/components/topNavigation.dart';
 import 'package:onboarding_animation/onboarding_animation.dart';
 
@@ -21,20 +21,20 @@ class Games extends StatelessWidget {
                   GamesContainerComponent(
                     image: "assets/pages/games/memory.png",
                     text: "Jeu de mémoire",
-                    gameName: "/jeu-memoire",
+                    gameName: "/jeux/jeu-memoire/tutoriel",
                   ),
                   GamesContainerComponent(
                       image: "assets/pages/games/logic.png",
                       text: "Jeu de logique",
-                      gameName: "/jeu-logique"),
+                      gameName: "/jeux/jeu-logique/tutoriel"),
                   GamesContainerComponent(
                       image: "assets/pages/games/quiz.png",
                       text: "Quiz",
-                      gameName: "/quiz"),
+                      gameName: "/jeux/quiz/tutoriel"),
                   GamesContainerComponent(
                       image: "assets/pages/games/treasure.png",
                       text: "Chasse au trésor",
-                      gameName: "/tresor")
+                      gameName: "/jeux/tresor")
                 ],
                 indicatorDotHeight: 10.0,
                 indicatorDotWidth: 15.0,
--- lib/screens/pass.dart
+++ lib/screens/pass.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, prefer_final_fields, avoid_print
 import 'package:flutter/material.dart';
 import 'package:vivatech/components/background.dart';
-import 'package:vivatech/components/qrCodeGenerator.dart';
+import 'package:vivatech/components/pass/qrCodeGenerator.dart';
 import 'package:vivatech/components/topNavigation.dart';
 
 class Pass extends StatefulWidget {
