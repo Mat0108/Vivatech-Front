f09551c69292e9e7255997ea64364a7c58285e20
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-05-24 00:51:53 +0200

    Update page_accueil

--- lib/accueil/accueil.dart
+++ lib/accueil/accueil.dart
@@ -22,105 +22,147 @@ class _Scene extends State<Scene> {
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
     return Scaffold(
-        body: Container(
-      height: 800,
-      width: 480,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/accueil/fond.png"),
-          fit: BoxFit.cover,
+      body: Container(
+        height: 800,
+        width: 480,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/accueil/fond.png"),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: 400,
-              height: 160,
-              margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10 * fem),
-                border: Border.all(color: const Color(0x33ffffff)),
-                color: const Color(0xa5ffffff),
-                image: const DecorationImage(
-                  image: AssetImage("assets/images/accueil/vivatech.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.fromLTRB(25 * fem, 40 * fem, 25 * fem, 0 * fem),
-              height: 102 * fem,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ButtonAccueil('assets/images/accueil/image-5.png', 'Mon pass',
-                      const Color(0xff00ffff), fem, ffem),
-                  ButtonAccueil('assets/images/accueil/image-19.png', 'Jeux',
-                      const Color(0xff00ff00), fem, ffem),
-                  ButtonAccueil('assets/images/accueil/image-20.png', 'Plan',
-                      const Color(0xffffff00), fem, ffem),
-                  ButtonAccueil('assets/images/accueil/image-8.png', 'CV',
-                      const Color(0xfff15700), fem, ffem),
-                ],
-              ),
-            ),
-            CarouselWithIndicatorDemo(type: "1"),
-            const CarouselWithIndicatorDemo(type: "2")
-          ],
-        ),
-      ),
-    ));
-  }
-
-  Widget ButtonAccueil(
-      String url, String titre, Color border, double fem, double ffem) {
-    return Container(
-        margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
-        width: 65 * fem,
-        height: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 7 * fem),
-              height: 70,
-              width: 70,
-              decoration: BoxDecoration(
-                border: Border.all(color: border),
-                color: const Color(0xb2ffffff),
-                borderRadius: BorderRadius.circular(50 * fem),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0x3f000000),
-                    offset: Offset(0 * fem, 4 * fem),
-                    blurRadius: 2 * fem,
-                  ),
-                ],
-              ),
-              child: Center(
-                child: SizedBox(
-                  width: 43 * fem,
-                  height: 40 * fem,
-                  child: Image.asset(
-                    url,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 400,
+                height: 160,
+                margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10 * fem),
+                  border: Border.all(color: const Color(0x33ffffff)),
+                  color: const Color(0xa5ffffff),
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/accueil/vivatech.png"),
                     fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ),
-            Container(
-              // monpassSE9 (1:41)
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 5 * fem, 0 * fem),
-              child: Text(
-                titre,
-                style:
-                    const TextStyle(fontSize: 14, color: VivatechColor.white),
+              Container(
+                margin: EdgeInsets.fromLTRB(25 * fem, 40 * fem, 25 * fem, 0 * fem),
+                height: 102 * fem,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ButtonAccueil('assets/images/accueil/image-5.png', 'Mon pass', const Color(0xff00ffff), fem, ffem),
+                    ButtonAccueil('assets/images/accueil/image-19.png', 'Jeux', const Color(0xff00ff00), fem, ffem),
+                    ButtonAccueil('assets/images/accueil/image-20.png', 'Plan', const Color(0xffffff00), fem, ffem),
+                    ButtonAccueil('assets/images/accueil/image-8.png', 'CV', const Color(0xfff15700), fem, ffem),
+                  ],
+                ),
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 40,
+                    child: Container(
+                      width: 30,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: VivatechColor.blue,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Carousel 1',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const CarouselWithIndicatorDemo(type: "1"),
+                ],
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 40,
+                    child: Container(
+                      width: 30,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: VivatechColor.blue,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Carousel 2',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const CarouselWithIndicatorDemo(type: "2"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget ButtonAccueil(
+    String url, String titre, Color border, double fem, double ffem) {
+  return Container(
+    margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
+    width: 65 * fem,
+    height: double.infinity,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 7 * fem),
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(
+            border: Border.all(color: border),
+            color: const Color(0xb2ffffff),
+            borderRadius: BorderRadius.circular(50 * fem),
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0x3f000000),
+                offset: Offset(0 * fem, 4 * fem),
+                blurRadius: 2 * fem,
+              ),
+            ],
+          ),
+          child: Center(
+            child: SizedBox(
+              width: 43 * fem,
+              height: 40 * fem,
+              child: Image.asset(
+                url,
+                fit: BoxFit.cover,
               ),
             ),
-          ],
-        ));
+          ),
+        ),
+        Container(
+          // monpassSE9 (1:41)
+          margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 5 * fem, 0 * fem),
+          child: Text(
+            titre,
+            style: const TextStyle(fontSize: 14, color: VivatechColor.white),
+          ),
+        ),
+      ],
+    ),
+  );
   }
 }
--- lib/accueil/carousel.dart
+++ lib/accueil/carousel.dart
@@ -35,62 +35,111 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 200,
-        width: 480,
-        child: Column(children: [
-          Expanded(
-            child: CarouselSlider(
-              items: (widget.type == "1" ? imgList : imgListStand)
-                  .map(
-                    (item) => Container(
-                      margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+    List<List<String>> imageLists = widget.type == "1" ? [imgList] : [imgListStand];
+    List<Widget> carouselItems = [];
+
+    for (List<String> imageList in imageLists) {
+      for (int i = 0; i < imageList.length; i += 2) {
+        Widget carouselItem = Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(0, 20, 4, 0),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(15)),
+                        image: DecorationImage(
+                          image: AssetImage(imageList[i]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      child: Center(
+                        child: Text(
+                          imageList[i]
+                              .toString()
+                              .split("/accueil/")[1]
+                              .split(".")[0],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: i + 1 < imageList.length
+                  ? Container(
+                      margin: const EdgeInsets.fromLTRB(4, 20, 0, 0),
                       child: Stack(
                         children: <Widget>[
                           Container(
                             decoration: BoxDecoration(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(15)),
+                              borderRadius: const BorderRadius.all(Radius.circular(15)),
                               image: DecorationImage(
-                                image: AssetImage(item),
+                                image: AssetImage(imageList[i + 1]),
                                 fit: BoxFit.cover,
                               ),
                             ),
                           ),
                           Positioned(
                             child: Center(
-                                child: Text(
-                              item
-                                  .toString()
-                                  .split("/accueil/")[1]
-                                  .split(".")[0],
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 30.0,
-                                fontWeight: FontWeight.bold,
+                              child: Text(
+                                imageList[i + 1]
+                                    .toString()
+                                    .split("/accueil/")[1]
+                                    .split(".")[0],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            )),
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  )
-                  .toList(),
+                    )
+                  : Container(),
+            ),
+          ],
+        );
+        carouselItems.add(carouselItem);
+      }
+    }
+
+    return Container(
+      height: 200,
+      width: 480,
+      child: Column(
+        children: [
+          Expanded(
+            child: CarouselSlider(
+              items: carouselItems,
               carouselController: _controller,
               options: CarouselOptions(
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                  aspectRatio: 3.0,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
-                  }),
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 3.0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: (widget.type == "1" ? imgList : imgListStand)
+            children: imageLists[0]
                 .asMap()
                 .entries
                 .map((entry) {
@@ -99,18 +148,20 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
                 child: Container(
                   width: 12.0,
                   height: 12.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 4.0),
+                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
                   decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black)
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4),
+                  ),
                 ),
               );
             }).toList(),
           ),
-        ]));
+        ],
+      ),
+    );
   }
 }
