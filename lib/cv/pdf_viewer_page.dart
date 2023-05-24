5c2b90965c380a587b058e345be8514e7097bf4c
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-05-24 14:33:33 +0200

    Update Cv

--- lib/cv/pdf_viewer_page.dart
+++ lib/cv/pdf_viewer_page.dart
@@ -1,77 +1,89 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:dio/dio.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_pdfview/flutter_pdfview.dart';
+// import 'package:front/main.dart';
+// import 'package:path/path.dart';
+// import 'package:path_provider/path_provider.dart';
 
-class PdfViewerPage extends StatefulWidget {
-  final String pdfUrl;
+// // class PdfViewerPage extends StatefulWidget {
+// //   final String pdfUrl;
 
-  const PdfViewerPage({super.key, required this.pdfUrl});
+// //   const PdfViewerPage({super.key, required this.pdfUrl,});
 
-  @override
-  _PdfViewerPageState createState() => _PdfViewerPageState();
-}
+// //   @override
+// //   _PdfViewerPageState createState() => _PdfViewerPageState();
+// // }
 
-class _PdfViewerPageState extends State<PdfViewerPage> {
-  late String localFilePath;
-  bool downloading = false;
-  bool fileExists = false;
+// // class _PdfViewerPageState extends State<PdfViewerPage> {
+// //   late String localFilePath;
+// //   bool downloading = false;
+// //   bool fileExists = false;
 
-  @override
-  void initState() {
-    super.initState();
-    checkFileExists().then((exists) {
-      setState(() {
-        fileExists = exists;
-      });
-    });
-  }
+// //   @override
+// //   void initState() {
+// //     super.initState();
+// //     checkFileExists().then((exists) {
+// //       setState(() {
+// //         fileExists = exists;
+// //       });
+// //     });
+// //   }
 
-  Future<bool> checkFileExists() async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String filePath = '${appDocDir.path}/myFile.pdf';
-    return File(filePath).exists();
-  }
+// //   Future<bool> checkFileExists() async {
+// //     Directory appDocDir = await getApplicationDocumentsDirectory();
+// //     String filePath = '${appDocDir.path}/myFile.pdf';
+// //     return File(filePath).exists();
+// //   }
 
-  Future<void> downloadFile() async {
-    setState(() {
-      downloading = true;
-    });
+// //   Future<void> downloadFile() async {
+// //     setState(() {
+// //       downloading = true;
+// //     });
 
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String appDocPath = appDocDir.path;
-    String filePath = '$appDocPath/myFile.pdf';
+// //     Directory appDocDir = await getApplicationDocumentsDirectory();
+// //     String appDocPath = appDocDir.path;
+// //     String filePath = '$appDocPath/myFile.pdf';
 
-    Dio dio = Dio();
-    await dio.download(widget.pdfUrl, filePath);
+// //     Dio dio = Dio();
+// //     await dio.download(widget.pdfUrl, filePath);
 
-    setState(() {
-      localFilePath = filePath;
-      downloading = false;
-      fileExists = true;
-    });
-  }
+// //     setState(() {
+// //       localFilePath = filePath;
+// //       downloading = false;
+// //       fileExists = true;
+// //     });
+// //   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PDF Viewer'),
-      ),
-      body: Center(
-        child: fileExists
-            ? PDFView(
-                filePath: localFilePath,
-              )
-            : downloading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: downloadFile,
-                    child: const Text('Télécharger PDF'),
-                  ),
-      ),
-    );
-  }
-}
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: const Text('PDF Viewer'),
+// //       ),
+// //       body: Center(
+// //         child: fileExists
+// //             ? PDFView(
+// //                 filePath: localFilePath,
+// //               )
+// //             : downloading
+// //                 ? const CircularProgressIndicator()
+// //                 : ElevatedButton(
+// //                     onPressed: downloadFile,
+// //                     child: const Text('Télécharger PDF'),
+// //                   ),
+// //       ),
+// //     );
+// //   }
+// // }
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,120 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:front/cv/pdf_viewer_page.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Test Demo',
       theme: ThemeData(fontFamily: "MuseoSans"),
-      home: const PdfViewerPage( pdfUrl: 'https://www.fluttercampus.com/sample.pdf'),
+      home: const MyHomePage(),
     );
-    
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-//   final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+class _MyHomePageState extends State<MyHomePage> {
+  String _fileText = "";
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("File Picker"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _pickMultipleFiles,
+                child: const Text("Pick Multiple Files"),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _pickDirectory,
+                child: const Text("Pick Directory"),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _saveAs,
+                child: const Text("Save As"),
+              ),
+              Text(_fileText),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
+  void _pickMultipleFiles() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(allowMultiple: true);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.titleLarge,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
+    if (result != null) {
+      List<File> files = result.paths.map((path) => File(path!)).toList();
+      setState(() {
+        _fileText = files.toString();
+      });
+    } else {
+      // Aucun fichier sélectionné
+    }
+  }
+
+  void _pickDirectory() async {
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    if (selectedDirectory != null) {
+      setState(() {
+        _fileText = selectedDirectory;
+      });
+    } else {
+      // Aucun dossier sélectionné
+    }
+  }
+
+  void _saveAs() async {
+    if (kIsWeb || Platform.isIOS || Platform.isAndroid) {
+      return;
+    }
+
+    Directory? appDocDir = await getExternalStorageDirectory();
+    if (appDocDir != null) {
+      String savePath = appDocDir.path + '/output_file.pdf';
+      String? outputFile = await FilePicker.platform.saveFile(
+        dialogTitle: 'Please select your CV',
+        fileName: savePath,
+      );
+
+      if (outputFile != null) {
+        File outputFileObj = File(outputFile);
+        await outputFileObj.copy(savePath);
+        setState(() {
+          _fileText = 'File saved at: $savePath';
+        });
+      } else {
+        // L'utilisateur a annulé la sauvegarde du fichier
+      }
+    }
+  }
+}
