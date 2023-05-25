e840225e40a29f0e4d335edcea781addad99cad8
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-05-25 17:19:22 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
-import 'components/cv/pdf_viewer_page.dart';
+import 'cv/pdf_viewer_page.dart';
 
 void main() {
   runApp(const MyApp());
